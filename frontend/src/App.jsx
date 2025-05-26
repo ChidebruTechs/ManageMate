d79e051b43706cb2b135fcfee5fcdc0b75697830
ChidebruTechs <chidebrutechs@gmail.com>
2025-05-26 07:34:38 -0400

    added a new line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className='align'>
     <h1>Hello World!</h1>
     <p>Nothing much to show: ManageMate under <strike>Construction</strike></p>
+    <p>Started Using Fedora Yesterday, Hurray!</p>
     </div>
   )
 }
